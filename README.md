181738e58553e900ead618c9cdd11d58fa024cf9
Bhukyapraveen2 <114331001+Bhukyapraveen2@users.noreply.github.com>
2024-05-06 17:52:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-f9txb4
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Bhukyapraveen2/react-f9txb4)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import LoveLetter from './components/LoveLetter/LoveLetter';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LoveLetter />
+    </div>
+  );
+}
+
+export default App;
